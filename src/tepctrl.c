e814a0c7642054e4abc00babc154e983d5f592b6
Charlie Blake <charlie@xyzebra.co.uk>
2011-12-26 17:30:21 +0000

    [src] Allow TEP machine to be controlled manually using an AVR
    Signed-off-by: Charlie Blake <charlie@xyzebra.co.uk>

--- src/tepctrl.c
+++ src/tepctrl.c
@@ -2,37 +2,75 @@
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 
-#define TIMER_THRESHOLD 0x4000
+#define TIMER_THRESHOLD 0x200
 
-static const uint8_t outputs[] = {
-	( _BV ( PD0 ) | _BV ( PD1 ) ),
-	( _BV ( PD1 ) | _BV ( PD2 ) ),
-	( _BV ( PD2 ) | _BV ( PD3 ) ),
-	( _BV ( PD3 ) | _BV ( PD0 ) ),
+#define BUTTON_PINS PINB
+#define BUTTON_LEFT PB1
+#define BUTTON_RIGHT PB2
+#define BUTTON_UP PB7
+#define BUTTON_DOWN PB0
+#define LED_X0 PD0
+#define LED_X1 PD1
+#define LED_X2 PD2
+#define LED_X3 PD3
+#define LED_Y0 PC0
+#define LED_Y1 PC1
+#define LED_Y2 PC2
+#define LED_Y3 PC3
+#define OUT_X PORTD
+#define OUT_Y PORTC
+#define IN_BUTTONS PORTB
+#define OUT_PINS_X DDRD
+#define OUT_PINS_Y DDRC
+
+static const uint8_t x_outputs[] = {
+	( _BV ( LED_X0 ) | _BV ( LED_X1 ) ),
+	( _BV ( LED_X1 ) | _BV ( LED_X2 ) ),
+	( _BV ( LED_X2 ) | _BV ( LED_X3 ) ),
+	( _BV ( LED_X3 ) | _BV ( LED_X0 ) ),
+};
+
+static const uint8_t y_outputs[] = {
+	( _BV ( LED_Y0 ) | _BV ( LED_Y1 ) ),
+	( _BV ( LED_Y1 ) | _BV ( LED_Y2 ) ),
+	( _BV ( LED_Y2 ) | _BV ( LED_Y3 ) ),
+	( _BV ( LED_Y3 ) | _BV ( LED_Y0 ) ),
 };
 
 ISR ( TIMER1_COMPA_vect ) {
-	static uint8_t position;
+	static uint8_t x_position;
+	static uint8_t y_position;
 	uint8_t buttons;
 
 	/* Read buttons and update position */
-	buttons = PINB;
-	if ( ! ( buttons & _BV ( PB2 ) ) )
-		position++;
-	if ( ! ( buttons & _BV ( PB1 ) ) )
-		position--;
+	buttons = BUTTON_PINS;
+
+	if ( ! ( buttons & _BV ( BUTTON_RIGHT ) ) )
+		x_position++;
+	if ( ! ( buttons & _BV ( BUTTON_LEFT ) ) )
+		x_position--;
+
+	if ( ! ( buttons & _BV ( BUTTON_UP ) ) )
+		y_position++;
+	if ( ! ( buttons & _BV ( BUTTON_DOWN ) ) )
+		y_position--;
 
 	/* Set outputs according to current position */
-	PORTD = outputs [ position % sizeof ( outputs ) ];
+	OUT_X = x_outputs [ x_position % sizeof ( x_outputs ) ];
+	OUT_Y = y_outputs [ y_position % sizeof ( y_outputs ) ];
 }
 
 int main ( void ) {
 
 	/* Configure input pins with internal pull-up resistors */
-	PORTB = ( _BV ( PB1 ) | _BV ( PB2 ) );
+	IN_BUTTONS = ( _BV ( BUTTON_LEFT ) | _BV ( BUTTON_RIGHT ) | _BV ( BUTTON_UP )| 
+		  _BV ( BUTTON_DOWN ) );
 
 	/* Configure output pins */
-	DDRD = ( _BV ( PD0 ) | _BV ( PD1 ) | _BV ( PD2 ) | _BV ( PD3 ) );
+	OUT_PINS_X = ( _BV ( LED_X0 ) | _BV ( LED_X1 ) | _BV ( LED_X2 ) | 
+		       _BV ( LED_X3 ) );
+	OUT_PINS_Y = ( _BV ( LED_Y0 ) | _BV ( LED_Y1 ) | _BV ( LED_Y2 ) | 
+		       _BV ( LED_Y3 ) );
 
 	/* Configure timer */
 	OCR1A = TIMER_THRESHOLD;
