3e95bbc29c9c064135e141e27c1b1d1f6ea99dd3
Charlie Blake <charlie@xyzebra.co.uk>
2012-02-26 04:26:35 +0000

    [src] Partially updated serial.c to handle Excellon file format
    Signed-off-by: Charlie Blake <charlie@xyzebra.co.uk>

--- src/rs232/serial.c
+++ src/rs232/serial.c
@@ -11,38 +11,162 @@
 #include "leds.h"
 #include "targets.h"
 #include "serial.h"
+#include <stdio.h>
 
-enum state { STATE_NONE, STATE_X, STATE_Y };
+enum state { STATE_NONE, STATE_C, STATE_G, STATE_M, STATE_T, STATE_X, STATE_Y, STATE_COMMENT };
+
+struct drillbits {
+	unsigned int diameter;
+};
+
+struct drillbits C_value[3] = {
+	{	
+		.diameter = 0,
+	},
+	{
+		.diameter = 0,
+	},
+	{
+		.diameter = 0,
+	},
+};		
 
 static enum state state;
 static unsigned int X_target;
 static unsigned int Y_target;
+static unsigned int G_value;
+static unsigned int M_value;
+static unsigned int T_value;
+static unsigned int value;
 
 /* When called, reset state and co-ordinate values */
 static void reset ( void ) {
 	state = STATE_NONE;
 	Y_target = 0;
 	X_target = 0;
+	G_value = 0;
+	M_value = 0;
+	T_value = 0;
 }
 
-/**
- * Handle received data
- *
- * @v data		Received character
- */
-static void receive ( char data ) {
-	uint8_t value;
+/* Handles data in state C */
+static void handle_c ( char data ) {
 
-	/* Set the RS232 activity counter */
-	rs232_activity_count = 5;
+	/* If data is a number, store it */
+	if ( ( data >= '0' ) && ( data <= '9' ) ) {
+		
+		/* Convert the character (data), into a number (value) */
+		value = data - '0';
+		
+		/* Add data to G_value */
+		C_value[T_value].diameter *= 10;
+		C_value[T_value].diameter += value;
+	}
 
-	/* If there is an error end process */
-	if ( error != 0 )
-		return;
+	else if ( data == '.' ) {
+	}
+
+	/* If data is a new line operator reset */
+	else if ( data == '\r' ) {
+		printf ( "Drillbit %d",  T_value ); 
+		printf (" diameter = %d\n", C_value[T_value].diameter );
+		reset ();
+
+	} else {
+		/* If data is unusable, set error */
+		error = 1;
+		reset();
+	}
+}
+
+/* Handles data in state G */
+static void handle_g ( char data ) {
+
+	/* If data is a number, store it */
+	if ( ( data >= '0' ) && ( data <= '9' ) ) {
+		
+		/* Convert the character (data), into a number (value) */
+		value = data - '0';
+		
+		/* Add data to G_value */
+		G_value *= 10;
+		G_value += value;
+	}
+
+	/* If data is a new line operator reset */
+	else if ( data == '\r' ) {
+		reset ();
+
+	} else {
+		/* If data is unusable, set error */
+		printf ( "G: %d\n",  G_value );
+		error = 1;
+		reset();
+	}
+}
+
+/* Handles data in state M */
+static void handle_m ( char data ) {
+		
+	/* If data is a number, store it */
+	if ( ( data >= '0' ) && ( data <= '9' ) ) {
+
+		/* Convert the character (data), into a number (value) */
+		value = data - '0';
+		
+		/* Add data to M_value */
+		M_value *= 10;
+		M_value += value;
+	}
+
+	/* If data is a new line operator reset */
+	else if ( data == '\r' ) {
+		printf ( "M: %d\n",  M_value );
+		reset ();
+
+	} else {
+		/* If data is unusable, set error */
+		error = 1;
+		reset();
+	}
+}
+
+/* Handles data in state T */
+static void handle_t ( char data ) {
 
 	/* If data is a co-ordinate number, store it */
 	if ( ( data >= '0' ) && ( data <= '9' ) ) {
+		
+		/* Convert the character (data), into a number (value) */
+		value = data - '0';
+		
+		/* Add data to X_target */
+			T_value *= 10;
+			T_value += value;
+	}
+	
+	/* If data is a C, set state to C */
+	else if ( data == 'C' ) {
+		state = STATE_C;
+	}
 
+	/* If data is a new line operator reset */
+	else if ( data == '\r' ) { 
+		reset ();
+
+	} else {
+		/* If data is unusable, set error */
+		error = 1;
+		reset();
+	}
+}
+
+/* Handles data in state X and Y */
+static void handle_x_and_y ( char data ) {
+
+	/* If data is a co-ordinate number, store it */
+	if ( ( data >= '0' ) && ( data <= '9' ) ) {
+		
 		/* Convert the character (data), into a number (value) */
 		value = data - '0';
 		
@@ -51,30 +175,19 @@ static void receive ( char data ) {
 			X_target *= 10;
 			X_target += value;
 		}
-
+		
 		/* If state is Y, add data to Y_target */
 		else if ( state == STATE_Y ) {
-				Y_target *= 10;
-				Y_target += value;
+			Y_target *= 10;
+			Y_target += value;
 		}
-
-		/* If the state is not X or Y, set an error and reset */
-		else {
-			error = 1;
-			reset ();
-		}	
 	}
-
-	/* If data is an X, set co-ordinate letter to X */
-	else if ( data == 'X' ) {
-		state = STATE_X;
-	}
-
-	/* If data is an Y, set co-ordinate letter to Y */
+	
+	/* If data is Y, set state to Y */
 	else if ( data == 'Y' ) {
 		state = STATE_Y;
 	}
-
+	
 	/* If data is a new line operator, store co-ordinates and reset */
 	else if ( data == '\r' ) {
 		targets [ target_prod ].x = X_target;
@@ -83,12 +196,79 @@ static void receive ( char data ) {
 		reset ();
 	}
 
-	/* If data is not useable, set an error and reset */
+	/* If data is unusable, set error */
 	else {
 		error = 1;
-		reset ();
+		reset();
+	}
+}
+
+/**
+ * Handle received data
+ *
+ * @v data		Received character
+ */
+static void receive ( char data ) {
+
+	/* Set the RS232 activity counter */
+	rs232_activity_count = 5;
+
+	/* If there is an error end process */
+	if ( error != 0 )
+		return;
+
+	if ( state == STATE_NONE ) {
+
+		/* If data is an G, set state to G */
+		if ( data == 'G' ) {
+			state = STATE_G;
+		}
+		
+		/* If data is an M, set state to M */
+		else if ( data == 'M' ) {
+			state = STATE_M;
+		}
+		
+		/* If data is an T, set state to T */
+		else if ( data == 'T' ) {
+			state = STATE_T;
+		}
+		
+		/* If data is an X, set state to X */
+		else if ( data == 'X' ) {
+			state = STATE_X;
+		}
+		
+		/* If data is an Y, set state to Y */
+		else if ( data == 'Y' ) {
+			state = STATE_Y;
+		}
 	}
 
+	/* If the state is C, process data */
+	else if ( state == STATE_C ) {
+		handle_c ( data );
+	}
+
+	/* If the state is G, process data */
+	else if ( state == STATE_G ) {
+		handle_g ( data );
+	}
+
+	/* If the state is M, process data */
+	else if ( state == STATE_M ) {
+		handle_m ( data );
+	}
+
+	/* If the state is T, process data */
+	else if ( state == STATE_T ) {
+		handle_t ( data );
+	}
+
+	/* If the state is X or Y, process data */
+	else if ( ( state == STATE_X ) || ( state == STATE_Y ) ) {
+		handle_x_and_y ( data );
+	}
 }
 
 /**
@@ -101,12 +281,36 @@ ISR ( USART_RX_vect ) {
 	/* Pick up character from serial port */
 	data = UDR0;
 
-	UDR0 = data;
-
 	/* Handle received data */
 	receive ( data );
 }
 
+/**
+ * Write character to serial port
+ *
+ * @c c			Character to write
+ */
+static int serial_putchar ( char c, FILE *stream ) {
+
+	/* Convert LF to CRLF */
+	if ( c == '\n' )
+		serial_putchar ( '\r', stream );
+ 
+	/* Wait for the transmit buffer to be empty */
+	while( ! ( UCSR0A & _BV ( UDRE0 ) ) ) {
+		/* Do nothing */
+	}
+ 
+	/* Transmit character */
+	UDR0 = c; 
+ 
+	return 0;
+}
+
+/* Define standard output stream */
+static FILE serial_stdout =
+	FDEV_SETUP_STREAM ( serial_putchar, NULL, _FDEV_SETUP_WRITE );
+ 
 /**
  * Initialise serial port
  *
@@ -122,4 +326,7 @@ void serial_init ( void ) {
 	UCSR0C = ( _BV ( UCSZ01 ) | _BV ( UCSZ00 ) );
 	UBRR0H = UBRRH_VALUE;
 	UBRR0L = UBRRL_VALUE;
+
+	/* Use serial port for stdout */
+	stdout = &serial_stdout;
 }
