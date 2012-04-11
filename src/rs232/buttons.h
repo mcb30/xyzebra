141beb8e29f39fe1df9636389ae02ed05726e50a
Charlie Blake <charlie@xyzebra.co.uk>
2012-04-11 17:22:41 +0100

    [src] Fixed sign of x and y directions
    Signed-off-by: Charlie Blake <charlie@xyzebra.co.uk>

--- src/rs232/buttons.h
+++ src/rs232/buttons.h
@@ -1,8 +1,8 @@
 #ifndef _BUTTONS_H
 #define _BUTTONS_H
 
-extern uint8_t manual_x;
-extern uint8_t manual_y;
+extern int8_t manual_x;
+extern int8_t manual_y;
 extern uint8_t manual_drill;
 
 extern void buttons_init ( void );
