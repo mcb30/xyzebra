dffde42218a8b2b78e261de55e90ba501056c621
Charlie Blake <charlie@xyzebra.co.uk>
2012-04-12 14:33:40 +0100

    [src] Updated with empty/full_targets functions Signed-off-by: Charlie Blake <charlie@xyzebra.co.uk>

--- src/rs232/targets.h
+++ src/rs232/targets.h
@@ -18,4 +18,14 @@ extern uint8_t target_prod;
 extern uint8_t target_cons;
 extern struct target targets[NUM_TARGETS];
 
+
+static inline int targets_full ( void ) {
+	return ( ( target_cons % NUM_TARGETS == target_prod % NUM_TARGETS ) && ( target_cons != target_prod ) );
+}
+
+static inline int targets_empty ( void ) {
+	return ( target_cons == target_prod );
+}
+
+
 #endif /* _TARGETS_H */
